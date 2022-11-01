95ecb01e1a2c18ca631fc5155036e76314252202
Pawaret Muengkaew <pawaret.muengkaew@outlook.com>
2022-11-01 16:31:50 +0700

    Implement first api

---
+++ src/api/user_api.rs
@@ -0,0 +1,32 @@
+use crate::{models::user_model::User, repository::mongodb_repo::MongoRepo};
+use mongodb::results::InsertOneResult;
+use rocket::{http::Status, serde::json::Json, State};
+
+#[post("/user", data = "<new_user>")]
+pub fn create_user(
+    db: &State<MongoRepo>,
+    new_user: Json<User>,
+) -> Result<Json<InsertOneResult>, Status> {
+    let data = User {
+        id: None,
+        address: new_user.address.to_owned(),
+    };
+    let user_detail = db.create_user(data);
+    match user_detail {
+        Ok(user) => Ok(Json(user)),
+        Err(_) => Err(Status::InternalServerError),
+    }
+}
+
+#[get("/user/<path>")]
+pub fn get_user(db: &State<MongoRepo>, path: String) -> Result<Json<User>, Status> {
+    let id = path;
+    if id.is_empty() {
+        return Err(Status::BadRequest);
+    };
+    let user_detail = db.get_user(&id);
+    match user_detail {
+        Ok(user) => Ok(Json(user)),
+        Err(_) => Err(Status::InternalServerError),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,40 @@
-use serde::{Serialize,Deserialize};
+mod api;
+mod jwt_sign;
+mod models;
+mod repository;
+
+use jwt_sign::create_jwt;
 use jwt_simple::prelude::*;
-fn main(){
-   
-  let key = HS256Key::generate();
-  println!(key)
+use std::fs::File;
+use std::io::{BufWriter, Write};
+
+#[macro_use]
+extern crate rocket;
+use api::user_api::{create_user, get_user};
+use repository::mongodb_repo::MongoRepo;
+use rocket::{get, http::Status, serde::json::Json};
+
+#[get("/")]
+fn index() -> Result<Json<String>, Status> {
+    Ok(Json(String::from("Hello from rust and mongoDB")))
+}
+
+#[launch]
+fn rocket() -> _ {
+    let db = MongoRepo::init();
+    rocket::build()
+        .manage(db)
+        .mount("/", routes![create_user])
+        .mount("/", routes![get_user])
+}
+
+fn login() {
+    let key = HS256Key::generate();
+    let byte_data = key.to_bytes();
+
+    let f = File::create("key").expect("Unable to create  file");
+    let mut f = BufWriter::new(f);
+    f.write_all(&byte_data).expect("Unable to write data");
+
+    print!("{}", create_jwt("someone@gmail.com".to_string()));
 }
