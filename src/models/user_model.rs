99316ced2c5bfd1bfc5d72054aeb812bae55b3e1
Pawaret Muengkaew <pawaret.muengkaew@outlook.com>
2022-11-02 18:37:06 +0700

    Update model

--- src/models/user_model.rs
+++ src/models/user_model.rs
@@ -5,7 +5,17 @@ use serde::{Deserialize, Serialize};
 pub struct User {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
     pub id: Option<ObjectId>,
-    pub username: String,
-    pub img: String,
+    pub username: Option<String>,
+    pub profile: Option<String>,
+    pub banner: Option<String>,
     pub address: String,
+    pub nonce: Option<i32>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CreateUser {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub id: Option<ObjectId>,
+    pub address: String,
+    pub nonce: Option<i32>,
 }
